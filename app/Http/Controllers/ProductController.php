18285502a8778b21a21455cfb48c37861a1cdffb
Nyein Mg <nyeinchanaung.myeik@gmail.com>
2022-01-14 17:07:08 +0630

    Add Product Api

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Product;
+
+class ProductController extends Controller
+{
+    public function addProduct(){
+        $product = new Product;
+        $product->name = request()->name;
+        $product->price = request()->price;
+        $product->description = request()->description;
+        $product->file_path = request()->file('file')->store('products');
+        $product->save();
+        return $product;
+    }
+}
